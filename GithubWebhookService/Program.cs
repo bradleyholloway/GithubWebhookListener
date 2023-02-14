30afd1b0460578cecde28ca9b505274d394f5b69
Bradley Holloway <bradley@holloway.fyi>
2023-02-14 15:57:24 -0600

    initial commit.

---
+++ GithubWebhookService/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace GithubWebhookService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var host = WebHost.Start("http://localhost", ProcessWebRequest))
+            {
+                Console.WriteLine("Use Ctrl-C to shutdown the host...");
+                host.WaitForShutdown();
+            }
+        }
+
+        public static async Task ProcessWebRequest(HttpContext app)
+        {
+            await app.Response.WriteAsync("Hello, World!");
+        }
+    }
+}
