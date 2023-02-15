9d6568e80754ebe29a86b5cad8dff82548e96b0d
Bradley Holloway <bradley@holloway.fyi>
2023-02-15 13:05:07 -0600

    Don't redirect output.

--- GithubWebhookService/Program.cs
+++ GithubWebhookService/Program.cs
@@ -64,14 +64,12 @@ namespace GithubWebhookService
                 System.Diagnostics.ProcessStartInfo procStartInfo =
                     new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
 
-                procStartInfo.RedirectStandardOutput = true;
                 procStartInfo.UseShellExecute = false;
                 procStartInfo.CreateNoWindow = true;
                 System.Diagnostics.Process proc = new System.Diagnostics.Process();
                 proc.StartInfo = procStartInfo;
                 proc.Start();
-                string result = proc.StandardOutput.ReadToEnd();
-                Console.WriteLine(result);
+                proc.WaitForExit();
             }
             catch (Exception)
             {
